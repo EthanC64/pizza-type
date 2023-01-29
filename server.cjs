68ff2848167516dcfd0e2788addf46904a8ade77
EthanC64 <64redbrick@gmail.com>
2023-01-29 10:28:38 -0800

    pizza

--- server.cjs
+++ server.cjs
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 
-const PORT = 6464;
+const PORT = process.env.PORT || 6464;
 
 const app = express();
 
